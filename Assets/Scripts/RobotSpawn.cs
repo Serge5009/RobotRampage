5b275c199a5b975806ac4ef31294628c3f90b7e5
Serhii <serge5709244@gmail.com>
2021-11-24 21:22:20 -0500

    Added robot spawn points

---
+++ Assets/Scripts/RobotSpawn.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotSpawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] robots;
+    private int timesSpawned;
+    private int healthBonus = 0;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SpawnRobot()
+    {
+        timesSpawned++;
+        healthBonus += 1 * timesSpawned;
+        GameObject robot = Instantiate(robots[Random.Range(0, robots.Length)]);
+        robot.transform.position = transform.position;
+        robot.GetComponent<Robot>().health += healthBonus;
+    }
+}
